f4e151ba9dac50b977937b93093ade50e23605cd
AquaticIntii11 <aquatic.intii@gmail.com>
2018-06-10 10:53:29 +0200

    adding some changes to master branch

--- src/Temp.java
+++ src/Temp.java
@@ -1,2 +1,10 @@
 public class Temp {
+    public String namex;
+    public int maxSpeedx;
+    private int currentSpeedx;
+
+    public void startEngine() {
+        this.currentSpeedx = 0;
+        System.out.println("Engine started");
+    }
 }

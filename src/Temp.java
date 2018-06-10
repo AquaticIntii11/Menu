e14ebde29ce142d4b88a49b3ffe2af2e178e2041
AquaticIntii11 <aquatic.intii@gmail.com>
2018-06-10 10:51:29 +0200

    adding some changes to conflict branch

--- src/Temp.java
+++ src/Temp.java
@@ -1,2 +1,10 @@
 public class Temp {
+    public String name;
+    public int maxSpeed;
+    private int currentSpeed;
+
+    public void startEngine() {
+        this.currentSpeed = 0;
+        System.out.println("Engine started");
+    }
 }
